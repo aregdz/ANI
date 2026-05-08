b301dabd4166de70bdcfe61e8b4f1944c683c75d
Арег <areg.dzharayan@bk.ru>
2026-05-08 17:34:29 +0300

    Fix Gmail SMTP for Railway

--- ani_album/settings.py
+++ ani_album/settings.py
@@ -94,13 +94,14 @@ ADMIN_PHONE = os.getenv('ANI_ADMIN_PHONE', '+79990000000')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = 'ani.memory.project@gmail.com'
 EMAIL_HOST_PASSWORD = 'fqqwfuvuxrftuyvv'
-
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'АНИ <ani.memory.project@gmail.com>'
 SERVER_EMAIL = EMAIL_HOST_USER
